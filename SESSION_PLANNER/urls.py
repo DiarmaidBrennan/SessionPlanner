e4623003da29901d3a12292d9298783873004c27
naraic78 <67059961+naraic78@users.noreply.github.com>
2020-06-18 10:50:11 +0100

    Add / to urls to point to /games

--- SESSION_PLANNER/urls.py
+++ SESSION_PLANNER/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('/', include('games.urls')),
     path('games/', include('games.urls')),
     path('sessionplanner/', include('sessionplanner.urls')),
     path('admin/', admin.site.urls),
